f2a37b809e851e4fa55c59138e2fcf25972044ba
ArtemSmit21 <smarde2103@mail.ru>
2025-03-17 15:35:40 +0300

    Add files via upload

---
+++ src/main/java/org/example/config/CassandraConnector.java
@@ -0,0 +1,27 @@
+package org.example.config;
+
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.CqlSessionBuilder;
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+import java.net.InetSocketAddress;
+
+@Getter
+@Component
+public class CassandraConnector {
+
+  private CqlSession session;
+
+  public void connect(String node, int port, String dataCenter) {
+    CqlSessionBuilder builder = CqlSession.builder();
+    builder.addContactPoint(new InetSocketAddress(node, port));
+    builder.withLocalDatacenter(dataCenter);
+    builder.withAuthCredentials("cassandra", "cassandra");
+    session = builder.build();
+  }
+
+  public void close() {
+    session.close();
+  }
+}
