32690dfa0b7e08cd06f745e52c3c6ae8368d494a
ArtemSmit21 <smarde2103@mail.ru>
2025-03-17 14:00:20 +0300

    Update CassandraDriverConfigLoaderBuilderCustomizer.java

--- src/main/java/org/example/config/CassandraDriverConfigLoaderBuilderCustomizer.java
+++ src/main/java/org/example/config/CassandraDriverConfigLoaderBuilderCustomizer.java
@@ -3,18 +3,18 @@ package org.example.config;
 import com.datastax.oss.driver.api.core.config.DefaultDriverOption;
 import com.datastax.oss.driver.api.core.config.ProgrammaticDriverConfigLoaderBuilder;
 import org.springframework.boot.autoconfigure.cassandra.DriverConfigLoaderBuilderCustomizer;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 
-@Configuration
+@Component
 public class CassandraDriverConfigLoaderBuilderCustomizer implements DriverConfigLoaderBuilderCustomizer {
 
   @Override
   public void customize(ProgrammaticDriverConfigLoaderBuilder programmaticDriverConfigLoaderBuilder) {
     programmaticDriverConfigLoaderBuilder
-      .withDuration(DefaultDriverOption.REQUEST_TIMEOUT, Duration.ofSeconds(20))
-      .withDuration(DefaultDriverOption.CONNECTION_CONNECT_TIMEOUT, Duration.ofSeconds(20))
-      .withDuration(DefaultDriverOption.CONNECTION_INIT_QUERY_TIMEOUT, Duration.ofSeconds(20));
+      .withDuration(DefaultDriverOption.REQUEST_TIMEOUT, Duration.ofSeconds(10))
+      .withDuration(DefaultDriverOption.CONNECTION_CONNECT_TIMEOUT, Duration.ofSeconds(10))
+      .withDuration(DefaultDriverOption.CONNECTION_INIT_QUERY_TIMEOUT, Duration.ofSeconds(10));
   }
 }
